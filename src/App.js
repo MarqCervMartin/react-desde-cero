d6aa8a5fdc41dbce1aa81da25f864218f94f6995
MarqCervMartin <martin_mcervantes@hotmail.com>
2021-02-06 17:58:53 -0600

    JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react'
+import "./styles/styles.scss"
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+  <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img alt="Banner" className="main-banner__img" src="https://s22.com.mx/revista/wp-content/uploads/2017/10/5886808-one-punch-man-wallpaper-hd.jpg"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Entendiendo REACT</p>
+          <p> Hola banner</p>
+          <a href="https://ed.team" className="button">Dale click</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+  <div className="ed-grid m-grid-3">
+    <Curso/>
+    <Curso/>
+    <Curso/>
+
+    <Curso/>
+    <Curso/>
+    <Curso/>
+    
+    <Curso/>
+    <Curso/>
+  </div>
+  </>
+)
 
 export default App;
+
+/*
+1 Regla : toda etiqueta debe cerrarse
+
+2 Regla : Los componentes deben devolver un elemento padre
+
+3 Regla: Apoyarse de los fragments cuando se necesita devolver dos elementos
+
+4: Fragmen 
+
+5: img siempre se cierra
+
+6: class => className
+
+7: for => htmlFor
+*/
