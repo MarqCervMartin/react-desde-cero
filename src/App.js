1b5c0a7fc6d55dad7bb61d96c075d11b7d3ad387
MarqCervMartin <martin_mcervantes@hotmail.com>
2021-02-07 13:57:43 -0600

    Arrays

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React, {Fragment} from 'react'
 import "./styles/styles.scss"
 import Curso from './Curso'
 
+const cursos = [
+  {
+    "title": "React desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/original/440bf729-4f2b-49a6-a0c7-7cf65a8bd31b.png",
+    "price": 30,
+    "profesor": "Mares"
+  },
+  {
+    "title": "React native",
+    "image": "https://edteam-media.s3.amazonaws.com/blogs/original/05ebfada-ca24-4d09-94e2-8a77aa63ee13.jpg",
+    "price": 100,
+    "profesor": "Alvaro"
+  },
+  {
+    "title": "CodeMX",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/original/59fbcbab-90ee-4971-b804-a29ca9d5f484.png",
+    "price": 30,
+    "profesor": "Jaime"
+  }
+]
+
 const App = () => (
   <>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -17,8 +38,9 @@ const App = () => (
     </div>
   </div>
   <div className="ed-grid m-grid-3">
-    <Curso
-    />
+    {
+      cursos.map( curso => <Curso title ={curso.title} image ={curso.image} price={curso.price} profesor={curso.profesor} />)
+    }
   </div>
   </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Curso = ({title, image, price, profesor}) => (
-    <article class="card">
-      <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+    <article className="card">
+      <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
         <img src={image} alt={title}/>
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">{title}</h3>
+        <h3 className="center">{title}</h3>
         <div className="s-main-center">
           {`Prof.: ${profesor}`}
         </div>
